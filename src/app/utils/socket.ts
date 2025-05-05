a86965adada5bc4471cfd3ab1ebe15e16eb61bee
suessmeister <josephsuess24@gmail.com>
2025-05-05 04:28:13 -0500

    semicolon errors can fuck up the server!

--- src/app/utils/socket.ts
+++ src/app/utils/socket.ts
@@ -2,7 +2,7 @@
 import { io, Socket } from "socket.io-client";
 
 let socket: Socket | null = null;
-const regular_server = "https://headlinedbackend-production.up.railway.app;"
+const regular_server = "https://headlinedbackend-production.up.railway.app"
 export function getSocket(): Socket {
    if (!socket) {
       if (typeof window !== "undefined") {
